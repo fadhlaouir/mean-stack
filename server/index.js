1c5b46cf10ea5bf5027040cd509d20d74c425e44
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-04-24 00:06:56 +0100

    add DB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,26 @@
+const mongoose = require("mongoose");
 const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
 
+dotenv.config();
 const PORT = 5000;
+
+// Connecting to DataBase
+mongoose.connect(
+    process.env.DATABASE, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Connected to the database");
+        }
+    }
+);
 
 app.get("/", (req, res) => {
     res.send("hi");
