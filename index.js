088259df3bdffa8bf1c528cc11783631ae1c9054
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-04-24 16:49:05 +0100

    fix err

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 /* ===================
    Import Node Modules
 =================== */
+const env = require("./env");
 const express = require("express"); // Fast, unopinionated, minimalist web framework for node.
 const app = express(); // Initiate Express Application
 const router = express.Router(); // Creates a new router object.
+const mongoose = require("mongoose"); // Node Tool for MongoDB
 const path = require("path"); // NodeJS Package for file paths
-const authentication = require("./routes/authentication")(router); // Import Authentication Routes
-const blogs = require("./routes/blogs")(router); // Import Blog Routes
+
 const bodyParser = require("body-parser"); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 const cors = require("cors"); // CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
+const port = process.env.PORT || 5000; // Allows heroku to set port
 const dotenv = require("dotenv");
-const mongoose = require("mongoose"); // Node Tool for MongoDB
 mongoose.Promise = global.Promise;
 
-const port = process.env.PORT || 5000; // Allows heroku to set port
 dotenv.config();
-
 // Connecting to DataBase
 mongoose.connect(
     process.env.DATABASE, {
@@ -31,11 +30,17 @@ mongoose.connect(
         }
     }
 );
+
 // Middleware
 app.use(cors({ origin: "http://localhost:4200" })); // Allows cross origin in development only
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(express.static(__dirname + "/public")); // Provide static directory for frontend
+
+//Route
+const authentication = require("./routes/authentication")(router); // Import Authentication Routes
+const blogs = require("./routes/blogs")(router); // Import Blog Routes
+
 app.use("/authentication", authentication); // Use Authentication routes in application
 app.use("/blogs", blogs); // Use Blog routes in application
 
@@ -44,7 +49,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-// Start Server: Listen on port 8080
+// Start Server
 app.listen(port, () => {
     console.log(`Listening on PORT ${port} in ${process.env.NODE_ENV} mode`);
 });
